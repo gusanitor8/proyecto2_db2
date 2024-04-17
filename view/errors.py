d10502db52ae3bb95511ee8dce3b6bddb838b23d
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2024-04-17 02:58:56 -0600

    Menu v2
    Menu con los arreglos de Usuario individuo y usuario empresa, para poder tener el dpi y nit como globals

---
+++ view/errors.py
@@ -0,0 +1,7 @@
+class Error(Exception):
+    """Clase base para excepciones en este módulo."""
+    pass
+
+class InvalidOptionError(Error):
+    """Excepción lanzada para opciones inválidas."""
+    pass
