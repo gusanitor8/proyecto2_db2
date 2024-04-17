7fe4640d9db35df5cb4b999b3dabb567449cf54d
gusanitor8 <gusanitor8@gmail.com>
2024-04-17 16:25:10 -0600

    function for deleting a node

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from controller.controller import run
+from controller.db_crud import delete_node
+from controller.db_connection import *
+
 
 if __name__ == "__main__":
-    run()
+#     run()
+    node_info = {
+        'labels': ['Individuo'],
+        'key_property': 'dpi',
+        'key_value': 5
+    }
+
+    try:
+        driver = get_driver()
+        with driver.session() as session:
+            delete_node(session, node_info)
+    except Exception as e:
+        print(f'Failed to delete node: {e}')
+    finally:
+        close_driver()
 
