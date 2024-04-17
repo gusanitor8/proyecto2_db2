7af4ae3005509d96c918e56c280628b4510a78db
LeivaDiego <lei21752@uvg.edu.gt>
2024-04-16 23:52:13 -0600

    Workspace cleanup

--- main.py
+++ main.py
@@ -3,33 +3,6 @@ from controller.db_crud import *
 
 try:
     driver = get_driver()
-    with driver.session() as session:
-        node1_info = {
-            'labels': ['Person', 'Employee'],
-            'properties': {'name': 'Alice', 'age': 30},
-            'key_property': 'name',
-            'key_value': 'Alice'
-        }
-
-        node2_info = {
-            'labels': ['Project'],
-            'properties': {'name': 'Neptune', 'description': 'Deep Learning'},
-            'key_property': 'name',
-            'key_value': 'Neptune'
-        }
-        node3_info = {
-            'labels': ['Project'],
-            'properties': {'name': 'XD', 'description': 'Dxdg'},
-            'key_property': 'name',
-            'key_value': 'XD'
-        }
-        relationship_type = 'WORKS_ON'
-        relationship_properties = {'since': '2020'}
-
-        print(create_node(session, node1_info))
-        print(create_node(session, node2_info))
-        print(create_relationship(session, node1_info, node2_info, relationship_type, relationship_properties))
-        print(create_relationship(session, node1_info, node3_info, relationship_type, relationship_properties))
 
 
 except Exception as e:
